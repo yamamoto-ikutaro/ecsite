af6b9bf564cec86416a5c2fea76edee172c40668
yamamoto.ikutaro <s-sxbx.37@outlook.jp>
2021-08-07 09:45:05 +0000

    formスタイル変更

--- resources/views/admin/admin_add_product.blade.php
+++ resources/views/admin/admin_add_product.blade.php
@@ -10,45 +10,45 @@
         <div>
         {!! Form::open(['route'=>'add_product', 'files' => true]) !!}
             <div class="form-group">
-                {!! Form::label('item_title', '商品名：') !!}
+                {!! Form::label('item_title', '商品名：', ['class'=>'text-right', 'style'=>'width:100px;']) !!}
                 {!! Form::text('item_title', null) !!}
                 @if($errors->first('item_title'))
                     <p class="text-danger">{{ $errors->first('item_title') }}</p>
                 @endif
             </div>
             <div class="form-group">
-                {!! Form::label('image', '画像：') !!}
+                {!! Form::label('image', '画像：', ['class'=>'text-right', 'style'=>'width:100px;']) !!}
                 {!! Form::file('image') !!}
                 @if($errors->first('image'))
                     <p class="text-danger">{{ $errors->first('image') }}</p>
                 @endif
             </div>
             <div class="form-group">
-                {!! Form::label('price', '値段：') !!}
+                {!! Form::label('price', '値段：', ['class'=>'text-right', 'style'=>'width:100px;']) !!}
                 {!! Form::number('price', null, ['class'=>'col-sm-1', 'min'=>0]) !!}
                 @if($errors->first('price'))
                     <p class="text-danger">{{ $errors->first('price') }}</p>
                 @endif
             </div>
             <div class="form-group">
-                {!! Form::label('category_id', 'カテゴリー：') !!}
+                {!! Form::label('category_id', 'カテゴリー：', ['class'=>'text-right', 'style'=>'width:100px;']) !!}
                 {!! Form::select('category_id', $categories, null) !!}
             </div>
             <div class="form-group">
-                {!! Form::label('item_quantity', '商品個数：') !!}
+                {!! Form::label('item_quantity', '商品個数：', ['class'=>'text-right', 'style'=>'width:100px;']) !!}
                 {!! Form::number('item_quantity', null, ['class' => 'col-sm-1', 'min'=>0]) !!}
                 @if($errors->first('item_quantity'))
                     <p class="text-danger">{{ $errors->first('item_quantity') }}</p>
                 @endif
             </div>
-            <div class="form-group">
-                {!! Form::label('content', '商品説明：') !!}
-                {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+            <div class="form-group row">
+                {!! Form::label('content', '商品説明：', ['class'=>'text-right', 'style'=>'width:115px; margin-right:3px;']) !!}
+                {!! Form::textarea('content', null, ['class'=>'col-sm-8']) !!}
                 @if($errors->first('content'))
                     <p class="text-danger">{{ $errors->first('content') }}</p>
                 @endif
             </div>
-            {!! Form::submit('追加', ['class' => 'btn btn-primary mb-4']) !!}
+            {!! Form::submit('追加', ['class' => 'btn btn-primary mb-4', 'style'=>'margin-left:102px;']) !!}
         {!! Form::close() !!}
         <div>
     @endif
--- resources/views/admin/admin_del_button.blade.php
+++ resources/views/admin/admin_del_button.blade.php
@@ -1,3 +1,3 @@
 {!! Form::open(['route' => ['admin_item.delete', $item->id], 'method' => 'delete']) !!}
-    {!! Form::submit('削除', ['class'=>'btn btn-danger']) !!}
+    {!! Form::submit('削除', ['class'=>'btn btn-danger mb-4', 'style'=>'margin-left:102px;']) !!}
 {!! Form::close() !!}
