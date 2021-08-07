cbaca8267c9e63c7f0df31cd76341bcc84af58b3
yamamoto.ikutaro <s-sxbx.37@outlook.jp>
2021-08-07 05:30:35 +0000

    seeder

---
+++ database/seeds/ItemsTableSeeder.php
@@ -0,0 +1,95 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+        DB::table('items')->insert([
+            'category_id' => 1,
+            'item_title' => 'test content',
+            'content' => 'test content',
+            'item_quantity' => 100,
+            'image_url' => 'blx6UjNnpQecx1Vt2kfcMhVe5wFiHohZ8PEFHkqd.jpg',
+            'price' => '100'
+        ]);
+    }
+}
